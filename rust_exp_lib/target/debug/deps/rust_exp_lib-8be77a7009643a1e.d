312f3bba141580a2e3f4d4db5e8a452e3a5f52ea
allenshi <allenshi@outlook.com>
2022-10-17 03:43:45 +0000

    ignore binary target artifact

--- rust_exp_lib/target/debug/deps/rust_exp_lib-8be77a7009643a1e.d
+++
@@ -1,8 +0,0 @@
-/opt/shared/projects/rustlang/rust_exp_lib/target/debug/deps/rust_exp_lib-8be77a7009643a1e: src/lib.rs src/utils.rs src/utils/collections.rs src/utils/collections/dlist.rs
-
-/opt/shared/projects/rustlang/rust_exp_lib/target/debug/deps/rust_exp_lib-8be77a7009643a1e.d: src/lib.rs src/utils.rs src/utils/collections.rs src/utils/collections/dlist.rs
-
-src/lib.rs:
-src/utils.rs:
-src/utils/collections.rs:
-src/utils/collections/dlist.rs:
